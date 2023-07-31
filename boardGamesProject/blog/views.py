427434dfff4e33c0807c7c119d424706a5fcf845
Eliza Gancarz <elizaganca@gmail.com>
2023-07-31 19:57:19 +0200

    adding post with image

---
+++ boardGamesProject/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+# from django.http import Http404
+from .models import BlogPost
+
+# Create your views here.
+
+def home(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+def detail(request, postId):
+    # try:
+    #     post = BlogPost.objects.get(id=postId)
+    # except BlogPost.DoesNotExist:
+    #     raise Http404("This article does not exist")
+    # else:
+    post = get_object_or_404(BlogPost, id=postId)
+    return render(request, 'detail.html', {'post':post})
