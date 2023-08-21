18041bd3914b1db30d513bcad93972551623b295
Eliza Gancarz <elizaganca@gmail.com>
2023-08-21 18:53:41 +0200

    added recently read section

--- boardGamesProject/blog/views.py
+++ boardGamesProject/blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-# from django.http import Http404
 from .models import BlogPost
 
 # Create your views here.
@@ -9,10 +8,19 @@ def home(request):
     return render(request, 'home.html', {'posts': posts})
 
 def detail(request, postId):
-    # try:
-    #     post = BlogPost.objects.get(id=postId)
-    # except BlogPost.DoesNotExist:
-    #     raise Http404("This article does not exist")
-    # else:
     post = get_object_or_404(BlogPost, id=postId)
-    return render(request, 'detail.html', {'post':post})
+    if 'recently_read' in request.session:
+        for (id, title) in request.session['recently_read']:
+            if post.id == id:
+                request.session['recently_read'].remove([id, title])
+        
+        request.session['recently_read'].insert(0, [post.id, post.title])
+
+        if len(request.session['recently_read']) > 4:
+            request.session['recently_read'].pop()
+    else:
+        request.session['recently_read'] = [[post.id, post.title]]
+
+    request.session.modified = True
+    
+    return render(request, 'detail.html', {'post':post, 'recently_read': request.session['recently_read'][1:]})
